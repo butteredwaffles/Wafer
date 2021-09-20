72a2b6497c67122a7781a71222e0e87b17ffc7e6
Nooqwaffle <jdonutiam@gmail.com>
2021-09-20 17:31:20 -0400

    Open and close garden feature

--- wafer.py
+++ wafer.py
@@ -13,18 +13,21 @@ class Wafer:
     GOLD_COOKIE_COLOR_1 = (193, 155, 71)
     GOLD_COOKIE_COLOR_2 = (225, 201, 111)
 
-    def __init__(self, manageGarden: bool = True):
+    gardenEnabled: bool
+
+    def __init__(self, gardenEnabled: bool = True):
         self.cookieCoords = None
         self._lock = threading.Lock()
         self.running = True
 
-        if manageGarden:
-            self.tendGarden()
+        self.gardenEnabled = gardenEnabled
 
     def run(self):
         self.calibrate()
         with ThreadPoolExecutor(max_workers=3) as executor:
-            futures = [executor.submit(self.clickMainCookie), executor.submit(self.clickGoldenCookies)]
+            futures = [executor.submit(self.clickMainCookie),
+                       executor.submit(self.clickGoldenCookies),
+                       executor.submit(self.runTasks)]
 
     def clickGoldenCookies(self):
         print("Beginning search for golden cookies.")
@@ -60,8 +63,31 @@ class Wafer:
         if not self.cookieCoords:
             print("Could not locate main cookie.")
 
+    def runTasks(self):
+        if self.gardenEnabled:
+            self.tendGarden()
+            self._openGarden()
+            time.sleep(5)
+            self._closeGarden()
+
     def tendGarden(self):
         farmLevel = 5  # TODO: Grab from save file.
         farm = Garden(_GARDEN_SAVE_TEST, farmLevel)
-        from pprint import pprint
-        pprint(farm.plots)
+
+    def _closeGarden(self):
+        for i in range(100):
+            coords = pyautogui.locateOnScreen("img/closeGarden.png", grayscale=True, confidence=0.8)
+            if coords:
+                with self._lock:
+                    pyautogui.click(coords)
+                return True
+        return False
+
+    def _openGarden(self):
+        for i in range(100):
+            coords = pyautogui.locateOnScreen("img/viewGarden.png", grayscale=True, confidence=0.8)
+            if coords:
+                with self._lock:
+                    pyautogui.click(coords)
+                return True
+        return False
